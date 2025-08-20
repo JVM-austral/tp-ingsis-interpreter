c340e562ad18ea2c58c83ed2b769a916f5c0702d
Mateo Serebrinsky <mserebrinsky@mail.austral.edu.ar>
2025-08-20 14:45:51 -0300

    Agrego el autotes para el lexer

---
+++ lexer/src/test/kotlin/lexertest/TestGenerator.kt
@@ -0,0 +1,27 @@
+package lexertest
+
+import com.google.gson.Gson
+import lexer.LexerImplementationV1
+import rules.*
+
+object TestGenerator {
+    private val analyzers = listOf(
+        KeywordAnalyzer(), NumberAnalyzer(), NumberTypeAnalyzer(),
+        OperatorAnalyzer(), PunctuationAnalyzer(), StringAnalyzer(), StringTypeAnalyzer(),
+        VariableAnalyzer(), WhitespaceAnalyzer(), MidStringAnalyzer(), MidNumberAnalyzer()
+    )
+
+    private val lexer = LexerImplementationV1(analyzers)
+    private val gson = Gson()
+
+    private fun generateJson(input: String): String {
+        val result = lexer.tokenize(input)
+        return gson.toJson(result)
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val input = if (args.isNotEmpty()) args.joinToString(" ") else ""
+        println(generateJson(input))
+    }
+}
--- token/src/main/kotlin/token/Token.kt
+++ token/src/main/kotlin/token/Token.kt
@@ -3,7 +3,7 @@ package token
 data class Token(val value: String, val type: TokenType) {
 
     fun isOfType(tokenType: TokenType): Boolean {
-        return this.type == tokenType
+        return this.type == tokenType;
     }
 
 }
