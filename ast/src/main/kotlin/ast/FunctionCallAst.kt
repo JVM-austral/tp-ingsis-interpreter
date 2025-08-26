e4a805528d27700cdbfd9532454aa96f509d8146
Mateo Serebrinsky <mserebrinsky@mail.austral.edu.ar>
2025-08-26 19:33:04 -0300

    Chore: Correction in functionCallAst

--- ast/src/main/kotlin/ast/FunctionCallAst.kt
+++ ast/src/main/kotlin/ast/FunctionCallAst.kt
@@ -1,12 +1,12 @@
 package ast
 
-class FunctionCallAst(private val name: String, private val parameters: List<Ast>, private val parametersNumber: Int) : Ast {
+class FunctionCallAst(private val name: String, private val parameters: List<Ast>) : Ast {
     override fun getListOfChildren(): List<Ast> {
         return parameters
     }
 
     override fun getChildLimit(): Int {
-        return parametersNumber
+        return parameters.size
     }
 
     override fun getValue(): String {
