5d4afab6c23df3d7d6151f7a8e160be2942f7569
Mateo Serebrinsky <mserebrinsky@mail.austral.edu.ar>
2025-09-18 16:26:00 -0300

    feat: cli supports env variables

---
+++ runner/src/test/kotlin/EnvAdapterTest.kt
@@ -0,0 +1,28 @@
+import envadapter.EnvAdapter
+import org.junit.jupiter.api.Test
+import runner.RunnerImplementation
+
+class EnvAdapterTest {
+
+    @Test
+    fun testEnvAdapter() {
+        val envAdapter = EnvAdapter()
+        val envMap: MutableMap<String, String> = System.getenv()
+        println(envAdapter.processEnv(envMap))
+    }
+
+    @Test
+    fun testEnvAdapterWithRunner(){
+        val envAdapter = EnvAdapter()
+        val envMap: MutableMap<String, String> = System.getenv()
+        val env = envAdapter.processEnv(envMap)
+        val runner = RunnerImplementation("V2", env = env)
+        val code = """ 
+            const a: string = readEnv("BEST_FOOTBALL_CLUB");
+            println(a);
+        """.trimIndent()
+
+        val codeStream = code.byteInputStream()
+        runner.run(codeStream)
+    }
+}
